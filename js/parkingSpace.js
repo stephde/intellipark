fe68a721d26b8cce63c83a3ecef7f6e35d1891d7
eLOPy <m.fergiani.m@gmail.com>
2015-03-17 23:28:09 +0100

    Imlpented parking grid

--- js/parkingSpace.js
+++ js/parkingSpace.js
@@ -1,1 +1,16 @@
 //takes the data in json format creates an appropriate table then applies a
+var rows = 10;
+var cols = 10;
+//create a table of size from the json
+
+var mapdata = {
+
+}
+
+for (var i = 0; i < rows; i++){
+	$("#parkingTable tbody").append("<tr></tr>");
+	for(var j = 0; j < cols; j++){
+		$("#parkingTable tr:last-child").append("<td class='freeSpace'></td>");
+	}
+	
+}
